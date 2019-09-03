e954d6ef6d0300fb46e1bd4dc0934fb0a712a718
jameszhenyu <772638102@qq.com>
2019-09-03 11:24:36 +0800

    Update crawl.py
    将数据写入mysql 数据库。

--- crawl.py
+++ crawl.py
@@ -2,7 +2,9 @@
 # -*- coding: utf-8 -*-
 
 from selenium import webdriver
-
+from sqlalchemy import create_engine
+from selenium.webdriver.common.keys import Keys
+import pymysql
 # 鼠标操作
 from selenium.webdriver.common.action_chains import ActionChains
 # 等待时间 产生随机数
@@ -17,11 +19,12 @@ from selenium.webdriver.support.ui import WebDriverWait
 def get_info():
 
     div_list = re.xpath('//*[@id="web-content"]/div/div[1]/div[2]/div[2]/div')
-    profession=re.xpath('//*[@id="search-filter"]/div[2]/div[1]/div/a[3]/text()')
-    location=re.xpath('//*[@id="search-filter"]/div[2]/div[1]/div/a[2]/text()')
-    profession=profession[0][5:]
-    location=location[0][3:]
-
+    profession = re.xpath(
+        '//*[@id="search-filter"]/div[2]/div[1]/div/a[3]/text()')
+    location = re.xpath(
+        '//*[@id="search-filter"]/div[2]/div[1]/div/a[2]/text()')
+    profession = profession[0][5:]
+    location = location[0][3:]
 
     # //*[@id="web-content"]/div/div[1]/div[2]/div[2]/div[1]/div/div[3]/div[3]/div[1]/span[2]/span
     # //*[@id="web-content"]/div/div[1]/div[2]/div[2]/div[6]/div/div[3]/div[4]/div/span[2]/span
@@ -29,20 +32,28 @@ def get_info():
     id_store = []
     time_store = []
     phone_store = []
-    loc=[]#地点
-    pro=[]#职业
+    loc = []  # 地点
+    pro = []  # 职业
     for i in div_list:
         name = i.xpath('div/div[3]/div[1]/a/text()')[0]
         id = i.xpath('div/div[3]/div[2]/div[1]/a/text()')
         if id == []:
             id = i.xpath('div/div[3]/div[3]/div[1]/a/text()')
+        if id == []:
+            id = 'none'
+
+        if isinstance(id, list):
+            id = id[0]
 
         time = i.xpath('div/div[3]/div[2]/div[3]/span/text()')
         if time == []:
             time = i.xpath('div/div[3]/div[3]/div[3]/span/text()')
+
         phone = i.xpath('div/div[3]/div[3]/div[1]/span[2]/span/text()')
         if phone == []:
             phone = i.xpath('div/div[3]/div[4]/div/span[2]/span/text()')
+        if isinstance(phone, list):  # 判断数据类型是不是为str，如果字符类型为list，抽有用字段
+            phone = phone[0]
 
         name_store.append(name)
         id_store.append(id)
@@ -55,11 +66,24 @@ def get_info():
                          '法定代表': np.array(id_store).reshape(-1),
                          '成立时间': np.array(time_store).reshape(-1),
                          '电话号码': np.array(phone_store).reshape(-1),
-                         '地点':np.array(loc),
-                         '行业':np.array(pro)})
+                         '地点': np.array(loc),
+                         '行业': np.array(pro)})
     print(data)
 
     Data_store.append(data)
+    data = pd.concat(Data_store, ignore_index=True)
+    data = data.drop_duplicates()
+    write_into_mysql(data)
+
+
+def write_into_mysql(data):
+
+    # DataFrame 写入数据库
+    conn = create_engine(
+        'mysql+pymysql://root:123456@localhost:3306/mysql',
+        encoding='utf8')
+    # 写入数据，table_name为表名，‘replace’表示如果同名表存在就替换掉
+    pd.io.sql.to_sql(data, "crawl_sub", conn, if_exists='append')
 
 
 if __name__ == '__main__':
@@ -70,7 +94,7 @@ if __name__ == '__main__':
     # 点击登录链接
     # loginLink = WebDriverWait(driver, 30).until(lambda x:x.find_element_by_xpath('//a[@οnclick="header.loginLink(event)"]'))
     loginLink = driver.find_element_by_css_selector(
-        '#web-content > div > div.container > div > div.login-right > div > div.module.module1.module2.loginmodule.collapse.in > div.title-tab.text-center > div.title.-active')
+        'body > div.tyc-header > div > div.right > div > div:nth-child(4) > a')
     print(loginLink.text)
     loginLink.click()
 
@@ -81,14 +105,15 @@ if __name__ == '__main__':
     login_by_pwd.click()
 
     # 输入用户名
-    username = driver.find_element_by_css_selector(
-        '#web-content > div > div.container > div > div.login-right > div > div.module.module1.module2.loginmodule.collapse.in > div.modulein.modulein1.mobile_box.f-base.collapse.in > div.pb30.position-rel > input')
-    username.send_keys('你的用户名')
+
+    username = driver.find_elements_by_css_selector(
+        "div.pb30.position-rel > input")[2]  # webelement 有四个只有一个可见
+    username.send_keys("13857102700")
 
     # 输入密码
     password = driver.find_element_by_css_selector(
-        'div.input-warp.-block > input.input.contactword')
-    password.send_keys('你的密码')
+        'div.input-warp.-block > input')
+    password.send_keys('abcde12345')
 
     login_button = driver.find_element_by_xpath('//div[text()="登录"]')
     login_button.click()
@@ -96,7 +121,16 @@ if __name__ == '__main__':
 
     html = driver.page_source
     re = etree.HTML(html)
-    page_num = re.xpath('//*[@id="web-content"]/div/div[1]/div[2]/div[3]/div/ul/li[12]/a/text()')
+
+
+    page_num = re.xpath(
+            '//*[@id="web-content"]/div/div[1]/div[2]/div[3]/div/ul/li[11]/a/text()')[0].strip('.')
+
+
+
+
+    page_num = int(page_num)
+
     print(page_num)
     get_info()
     time.sleep(3)
@@ -122,31 +156,30 @@ if __name__ == '__main__':
         print(i)
     print('前5页')
 
-    for j in range(150):
-        if j >10:
+    for j in range(4, page_num - 7):
+        if j > 10:
             time.sleep(4)
-
-
-
 
         for link in driver.find_elements_by_xpath(  # https://antirobot.tianyancha.com/captcha/verify?return_url=https%3A%2F%2Fwww.tianyancha.com%2Fsearch%2Foe01-s2-hp1%2Fp114%3Fbase%3Dhangzhou&rnd=
                 '//*[@id="web-content"]/div/div[1]/div[2]/div[3]/div/ul/li[14]/a'):
-            # url = driver.current_url
-            # print(url)
-            #url = 'https://antirobot.tianyancha.com/captcha/verify?return_url=https%3A%2F%2Fwww.tianyancha.com%2Fsearch%2Foe01-s2-hp1%2Fp102%3Fbase%3Dhangzhou&rnd='
-
 
             href = (link.get_attribute('href'))
             driver.get(href)
-
-
             html = driver.page_source
             re = etree.HTML(html)
             get_info()
         print(j)
 
+    for k in range(page_num - 7, page_num - 2):
+        if k > 10:
+            time.sleep(4)
 
-
-
-    data = pd.concat(Data_store, ignore_index=True)
-    data.to_csv('C:\\Users\\Administrator\\Desktop\\w\\data3.csv', index=False)
+        for link in driver.find_elements_by_xpath(
+                # https://antirobot.tianyancha.com/captcha/verify?return_url=https%3A%2F%2Fwww.tianyancha.com%2Fsearch%2Foe01-s2-hp1%2Fp114%3Fbase%3Dhangzhou&rnd=
+                '//*[@id="web-content"]/div/div[1]/div[2]/div[3]/div/ul/li[13]/a'):
+            href = (link.get_attribute('href'))
+            driver.get(href)
+            html = driver.page_source
+            re = etree.HTML(html)
+            get_info()
+        print(k)
